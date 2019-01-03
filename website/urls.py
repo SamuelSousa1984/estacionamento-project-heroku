03106cc05dc7aae93798873d477871a4f0ed6a5b
Samuel Sousa <samuelsousapereira@gmail.com>
2019-01-03 16:40:40 -0200

    Carregando Bootstrap na página web

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, re_path, include
-from .views import index
+from .views import index, contato, sobre, servico, planos
+
 
 urlpatterns = [
-    path('', index, name='website_home')
+    path('', index, name='website_home'),
+    path('', contato, name='website_contato'),
+    path('', planos, name='website_planos'),
+    path('', sobre, name='website_sobre'),
+    path('', servico, name='website_servico'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'FrontEndFiles/index.html')
+
+
+def contato(request):
+    return render(request, 'FrontEndFiles/contatos.html')
+
+
+def planos(request):
+    return render(request, 'FrontEndFiles/planos.html')
+
+
+def sobre(request):
+    return render(request, 'FrontEndFiles/sobre.html')
+
+
+def servico(request):
+    return render(request, 'FrontEndFiles/serviços.html')
